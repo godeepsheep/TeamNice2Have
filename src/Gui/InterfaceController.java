dd72e39f6aa9cdc39942f2f3d0177ae09430e929
EpicoModeo <Mark-Nielsen@outlook.com>
2024-01-09 15:21:18 +0100

    InterfaceController Added

---
+++ src/Gui/InterfaceController.java
@@ -0,0 +1,42 @@
+package Gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+import java.awt.*;
+
+public class InterfaceController {
+
+    @FXML
+    TextField Team1Name;
+    @FXML
+    TextField Team2Name;
+    @FXML
+    TextField Team1Score;
+    @FXML
+    TextField Team2Score;
+    @FXML
+    TextField Timer;
+    @FXML
+    Button Team1Goal;
+    @FXML
+    Button Team2Goal;
+    @FXML
+    Button Team1Penalty;
+    @FXML
+    Button Team2Penalty;
+    @FXML
+    ImageView PauseButton;
+
+
+    public void AddGoal(String Team){
+
+    }
+
+    public void AddPenalty(String Team){
+
+    }
+
+
+
+}
