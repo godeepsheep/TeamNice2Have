e64f7fd3f7d1f12708fea143c225859a817f3cf5
godeepsheep <124163040+godeepsheep@users.noreply.github.com>
2024-01-09 23:02:29 +0100

    Made classes from the ER diagram.

---
+++ src/Data/Event.java
@@ -0,0 +1,10 @@
+package Data;
+
+public class Event {
+    //Attributes
+    private int eventID;
+    private int typeID;
+    private int matchID;
+    private int teamID;
+    private String time;
+}
