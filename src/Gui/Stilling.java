afa07474a7fc64fb36aa23705a8f940907693834
GitMarkNielsen <Mark-Nielsen@outlook.com>
2024-01-10 09:39:17 +0100

    deleted .ideaworkspace from gitignore

--- src/Gui/Stilling.java
+++ src/Gui/Stilling.java
@@ -2,7 +2,9 @@ package Gui;
 
 import javafx.fxml.FXML;
 
-import javax.swing.table.TableColumn;
+
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class Stilling {
 
@@ -17,13 +19,13 @@ public class Stilling {
     TableColumn Pts;
     @FXML
     TableColumn Goals;
-
+    @FXML
+    TableView tableView;
     public void AddEntry(String _Stilling, String _HoldNavn, String _Kampe, String _Pts, String _Goals){
-    Stilling.setHeaderValue(_Stilling);
-    HoldNavn.setHeaderValue(_HoldNavn);
-    Kampe.setHeaderValue(_Kampe);
-    Pts.setHeaderValue(_Pts);
-    Goals.setHeaderValue(_Goals);
+
+
+    tableView.setItems();
+
     }
     public void AddEntry(String _HoldNavn) {
         Stilling.setHeaderValue("");
