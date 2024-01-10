7916d7d817bd7b1b7041a936d88ab707c20446cb
GitMarkNielsen <Mark-Nielsen@outlook.com>
2024-01-10 10:52:50 +0100

    Stilling scene test-functional

--- src/Gui/Stilling.java
+++ src/Gui/Stilling.java
@@ -1,10 +1,16 @@
 package Gui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableSet;
 import javafx.fxml.FXML;
 
 
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.ArrayList;
 
 public class Stilling {
 
@@ -16,15 +22,27 @@ public class Stilling {
     @FXML
     TableColumn Kampe;
     @FXML
-    TableColumn Pts;
-    @FXML
     TableColumn Goals;
     @FXML
+    TableColumn Pts;
+    @FXML
     TableView tableView;
-    public void AddEntry(String _Stilling, String _HoldNavn, String _Kampe, String _Pts, String _Goals){
+    public void AddEntry(){
+
+        final ObservableList<StandingEntry> data = FXCollections.observableArrayList(
+                //Get data from database and enter it here.
+                new StandingEntry("1", "Hello World!", "1", "1", "1"),
+                new StandingEntry("2", "Hello World!", "1", "1", "1")
+        );
 
 
-    
+        Stilling.setCellValueFactory(new PropertyValueFactory<StandingEntry,String>("Standing"));
+        HoldNavn.setCellValueFactory(new PropertyValueFactory<StandingEntry,String>("Name"));
+        Kampe.setCellValueFactory(new PropertyValueFactory<StandingEntry,String>("Matches"));
+        Goals.setCellValueFactory(new PropertyValueFactory<StandingEntry,String>("GoalDiff"));
+        Pts.setCellValueFactory(new PropertyValueFactory<StandingEntry,String>("Points"));
+
+        tableView.setItems(data);
 
     }
     public void AddEntry(String _HoldNavn) {
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import Data.Student;
 import Logic.Liga;
 import Logic.TeamScore;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -16,9 +17,11 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("Gui/application.css").toExternalForm());
+//			BorderPane root = new BorderPane();
+			FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Gui/Stilling.fxml"));
+			Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+			//Scene scene = new Scene(root,400,400);
+			//scene.getStylesheets().add(getClass().getResource("Gui/application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
