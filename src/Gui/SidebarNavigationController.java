fea6612e7988297b1ebcd89aaa9f70296e3c23a8
sbaaps <stinebondeaagaard@hotmail.com>
2024-01-11 11:16:01 +0100

    Navn på billeder ændret

--- src/Gui/SidebarNavigationController.java
+++ src/Gui/SidebarNavigationController.java
@@ -1,15 +1,12 @@
 package Gui;
 
-import Data.Datalayer;
 import javafx.animation.FadeTransition;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 import javafx.util.Duration;
 
 import java.net.URL;
@@ -22,7 +19,6 @@ public class SidebarNavigationController implements Initializable{
 
         @FXML
         private ImageView menu;
-
 
 
         @Override
