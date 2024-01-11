67cdbdbfbd040040fa2c7c3409b06b934f21aaad
sbaaps <stinebondeaagaard@hotmail.com>
2024-01-11 13:11:19 +0100

    MouseEvent cursor

--- src/Gui/SidebarNavigationController.java
+++ src/Gui/SidebarNavigationController.java
@@ -4,7 +4,9 @@ import javafx.animation.FadeTransition;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
@@ -20,51 +22,70 @@ public class SidebarNavigationController implements Initializable{
         @FXML
         private ImageView menu;
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        setupTransitions();
 
-        @Override
-        public void initialize(URL location, ResourceBundle resources) {
-            pane1.setVisible(false);
+        //Actions for cursorchange when over menu
+        menu.setOnMouseEntered(event -> setCursorHand());
+        menu.setOnMouseExited(event -> setCursorDefault());
+        menu.setOnMousePressed(event -> setCursorDefault());
+        menu.setOnMouseReleased(event -> setCursorHand());
 
+        menu.setOnMouseClicked(event -> showPane1());
 
-            FadeTransition fadeTransition=new FadeTransition(Duration.seconds(0.5),pane1);
-            fadeTransition.setFromValue(1);
-            fadeTransition.setToValue(0);
-            fadeTransition.play();
+        pane1.setOnMouseEntered(event -> setCursorHand());
+        pane1.setOnMouseExited(event -> setCursorDefault());
+        pane1.setOnMouseClicked(event -> hidePane1());
+    }
 
-            TranslateTransition translateTransition=new TranslateTransition(Duration.seconds(0.5), pane2);
-            translateTransition.setByX(-600);
-            translateTransition.play();
+    private void setupTransitions() {
+        pane1.setVisible(false);
 
-            menu.setOnMouseClicked(event -> {
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(0.5), pane1);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(0);
 
-                pane1.setVisible(true);
+        TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(0.5), pane2);
+        translateTransition.setByX(-600);
 
-                FadeTransition fadeTransition1=new FadeTransition(Duration.seconds(0.5),pane1);
-                fadeTransition1.setFromValue(0);
-                fadeTransition1.setToValue(0.15);
-                fadeTransition1.play();
+        fadeTransition.play();
+        translateTransition.play();
+    }
 
-                TranslateTransition translateTransition1=new TranslateTransition(Duration.seconds(0.5),pane2);
-                translateTransition1.setByX(+600);
-                translateTransition1.play();
-            });
+    private void setCursorHand() {
+        menu.getScene().setCursor(Cursor.HAND);
+    }
 
-            pane1.setOnMouseClicked(event -> {
+    private void setCursorDefault() {
+        menu.getScene().setCursor(Cursor.DEFAULT);
+    }
 
-                FadeTransition fadeTransition1=new FadeTransition(Duration.seconds(0.5),pane1);
-                fadeTransition1.setFromValue(0.15);
-                fadeTransition1.setToValue(0);
-                fadeTransition1.play();
+    private void showPane1() {
+        pane1.setVisible(true);
 
-                fadeTransition1.setOnFinished(event1 -> {
-                    pane1.setVisible(false);
-                });
+        FadeTransition fadeTransition1 = new FadeTransition(Duration.seconds(0.5), pane1);
+        fadeTransition1.setFromValue(0);
+        fadeTransition1.setToValue(0.15);
 
-                TranslateTransition translateTransition1=new TranslateTransition(Duration.seconds(0.5),pane2);
-                translateTransition1.setByX(-600);
-                translateTransition1.play();
-            });
-        }
-   }
+        TranslateTransition translateTransition1 = new TranslateTransition(Duration.seconds(0.5), pane2);
+        translateTransition1.setByX(+600);
 
+        fadeTransition1.play();
+        translateTransition1.play();
+    }
+    
+    private void hidePane1() {
+        FadeTransition fadeTransition1 = new FadeTransition(Duration.seconds(0.5), pane1);
+        fadeTransition1.setFromValue(0.15);
+        fadeTransition1.setToValue(0);
 
+        fadeTransition1.setOnFinished(event1 -> pane1.setVisible(false));
+
+        TranslateTransition translateTransition1 = new TranslateTransition(Duration.seconds(0.5), pane2);
+        translateTransition1.setByX(-600);
+
+        fadeTransition1.play();
+        translateTransition1.play();
+    }
+}
