7251ce03b6d86f16c5613d6edbf559aad3806a4e
hplaursen <email@hanspeter.dk>
2024-01-14 00:22:00 +0100

    main

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import Data.Datalayer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
@@ -38,6 +39,8 @@ public class Main extends Application {
 	double x,y = 0;
 	@Override
 	public void start(Stage primaryStage) throws Exception{
+
+		primaryStage.getIcons().add(new Image("/Gui/images/ball.png"));
 
 		//String fxmlfile = "Interface";
 		String fxmlfile = "SidebarNavigation";
