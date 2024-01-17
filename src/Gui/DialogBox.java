1c2bc7db8b62ade0c13c6e26ec4b2db2edb2dcce
hplaursen <email@hanspeter.dk>
2024-01-17 11:19:47 +0100

    fix

--- src/Gui/DialogBox.java
+++ src/Gui/DialogBox.java
@@ -18,7 +18,7 @@ public class DialogBox {
             TextInputDialog inputdialog = new TextInputDialog(defaultValue);
             inputdialog.setTitle("Opret Hold");
             inputdialog.setHeaderText("Indtast holdnavn");
-            inputdialog.setContentText("Navns:");
+            inputdialog.setContentText("Navn:");
 
             Optional<String> result = inputdialog.showAndWait();
             StringResult = result.orElse(defaultValue);
